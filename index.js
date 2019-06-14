9c6a7e9f9933eb22e28943edf29ac2f42747830e
Janet Mendez <janetmendez.03@gmail.com>
2019-06-14 20:44:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // add solution here
+function theBeatlesPlay(arr1, arr2){
+  var fullArr = [];
+  
+  for(var i = 0; i < arr1.length; i++){
+    var phrase = `${arr1[i]} plays ${arr2[i]}`;
+    fullArr.push(phrase);
+  }
+  return fullArr
+}
+
+function johnLennonFacts(arr){
+  var n = 0;
+  var loudFacts= [];
+  while(n < arr.length ){
+    loudFacts.push(`${arr[n]}!!!`);
+  }
+  return loudFacts;
+}
